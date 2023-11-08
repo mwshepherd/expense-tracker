91e40b73943fc0577cd52065dbd0c4dbf3382c18
Michael Shepherd <53623238+mwshepherd@users.noreply.github.com>
2023-11-08 23:49:05 +1100

    fix: remove height from empty and loading states

--- src/components/Listings/EmptyState.tsx
+++ src/components/Listings/EmptyState.tsx
@@ -5,7 +5,7 @@ export const EmptyState = ({ currentTab }: { currentTab: 'expenses' | 'income' }
   const { setModalOpen } = useModalContext()
 
   return (
-    <div className="h-[calc(100vh_-_254px)] flex flex-col items-center justify-center gap-4 h-full">
+    <div className="h-[calc(100vh_-_254px)] flex flex-col items-center justify-center gap-4">
       <p className="text-gray-500 uppercase font-italic">No {currentTab === 'expenses' ? 'expenses' : 'income'} to show</p>
       <Button onClick={() => setModalOpen(true)}>Add {currentTab === 'expenses' ? 'Expense' : 'Income'}</Button>
     </div>
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,7 +1,7 @@
 import { ArrowPathIcon } from '@heroicons/react/24/outline'
 
 export const Loader = () => (
-  <div className="h-[calc(100vh_-_254px)] flex flex-col items-center justify-center gap-4 h-full">
+  <div className="h-[calc(100vh_-_254px)] flex flex-col items-center justify-center gap-4">
     <p className="text-gray-500 uppercase font-italic">Loading...</p>
 
     <div className="animate-spin w-8 h-8 rounded-full">
